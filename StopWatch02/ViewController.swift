244771083cdbb1e3d38ed5a5e3b74927bc1785f0
사용자 방문 <Guest@d7703-29ui-imac.local>
2019-04-04 14:15:06 +0900

    Initial Commit

---
+++ StopWatch02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StopWatch02
+//
+//  Created by 방문 사용자 on 2019. 4. 4..
+//  Copyright © 2019년 방문 사용자. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StopWatch02Tests/StopWatch02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  StopWatch02Tests.swift
+//  StopWatch02Tests
+//
+//  Created by 방문 사용자 on 2019. 4. 4..
+//  Copyright © 2019년 방문 사용자. All rights reserved.
+//
+
+import XCTest
+@testable import StopWatch02
+
+class StopWatch02Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
