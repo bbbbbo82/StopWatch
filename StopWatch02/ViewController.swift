c05d694e5eede721bfb26eade66c6ff33e0df1e0
사용자 방문 <Guest@d7703-29ui-imac.local>
2019-04-04 16:27:54 +0900

    complete

--- StopWatch02/ViewController.swift
+++ StopWatch02/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var time = 0
+    var min = 0
+    var sec = 0
+    
+    //Timaer 객체생성
+    var myTimer = Timer()
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIBarButtonItem!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    @IBOutlet weak var stopButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = String .localizedStringWithFormat("%02d:%02d.%02d", min, sec, time)
     }
-
-
+    
+    @IBAction func startBtnPressed(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        changeButtonState(start: false, pause: true, stop: true)
+    }
+    
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        //Timer 정지 기능
+        myTimer.invalidate()
+        changeButtonState(start: true, pause: false, stop: true)
+    }
+    
+    @IBAction func restartBtnPressed(_ sender: Any) {
+        myTimer.invalidate()
+        time = 0
+        timeLabel.text = String(time)
+        changeButtonState(start: true, pause: false, stop: false)
+    }
+    
+    @objc func updateTime(){
+        time += 1
+        timeLabel.text = String .localizedStringWithFormat("%02d:%02d.%02d", min, sec, time)
+        
+        if(time == 100){
+            time = 0
+            sec += 1
+        }
+        
+        if(sec == 60){
+            sec = 0
+            min += 1
+        }
+    }
+    
+    func changeButtonState(start:Bool, pause:Bool, stop:Bool){
+        startButton.isEnabled = start
+        pauseButton.isEnabled = pause
+        stopButton.isEnabled = stop
+    }
 }
 
